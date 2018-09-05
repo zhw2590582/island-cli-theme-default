4a972d485c7c0c28001824c7cca974a5279c2104
Harvey Zack <laozhaochaguan@hotmail.com>
2018-09-05 21:24:18 +0800

    update

--- js/message.js
+++ js/message.js
@@ -1,5 +1,10 @@
+import Gitting from 'gitting';
 import { setTitle } from "./utils";
 import "../sass/pages";
 import "../sass/message";
+import "gitting/dist/gitting.css";
+import { web } from "../../../data/config"
 
 setTitle('留言');
+const gitting = new Gitting(web.gitting);
+gitting.render('#gitting-container');
